a9b40f281d247dcff9bc88cae604ae514464f73f
Artem <artem.v.maluga@gmail.com>
2021-02-14 22:42:02 +0300

    Add few assert checks for validate action

--- tests/arbitrage/suites/validate_tests.hpp
+++ tests/arbitrage/suites/validate_tests.hpp
@@ -16,8 +16,16 @@ try {
     WASM_ASSERT("validate: expected_balance should be positive",
                 arb.validate(N(mindswaparbi), N(swap), N(mindswaparbi), negative_iq_token_asset, N(alice)));
 
-     WASM_ASSERT("validate: swap validation failed for mindswaparbi 200.000 IQ!>=1000.000 IQ",
+    WASM_ASSERT("validate: swap validation failed for mindswaparbi 200.000 IQ!>=1000.000 IQ",
                 arb.validate(N(mindswaparbi), N(swap), N(mindswaparbi), iq_token_asset, N(alice)));
+
+    extended_asset sys_token_asset{asset::from_string("199.970 SYS"), iq.contract};
+
+    WASM_ASSERT("validate: recipient account is not exist",
+                arb.validate(N(mindswaparbi), N(swap), N(mindswaparbi), sys_token_asset, N(notexisten)));
+
+    WASM_ASSERT("validate: withdraw account is not exist",
+                arb.validate(N(mindswaparbi), N(swap), N(mindswaparbi), sys_token_asset, N(alice)));
 }
 FC_LOG_AND_RETHROW()
 
